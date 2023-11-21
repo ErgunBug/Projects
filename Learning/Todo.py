04d9b07793a519e39def30309967ee28ffe970e9
ErgunBug <149547243+ErgunBug@users.noreply.github.com>
2023-11-21 20:58:08 +0100

    Update Todo.py
    Removed Delete button

--- Learning/Todo.py
+++ Learning/Todo.py
@@ -6,10 +6,6 @@ window.title("To Do Liste")
 ctk.set_appearance_mode("dark")
 
 
-def update_listbox():
-        # Clear the listbox
-    self.listbox.delete(0, ctk.END)
-
 def button():
     todo = entry.get()
     label = ctk.CTkLabel(scroll, text=todo)
@@ -27,8 +23,6 @@ entry.pack()
 add_button = ctk.CTkButton(window, text="Add", text_color="black", width=600, fg_color="white", command=button)
 add_button.pack()
 
-delete_button = ctk.CTkButton(window, text="Delete", text_color="white", width=600, hover_color="red", fg_color="black")
-delete_button.pack(pady="10px")
 
 
 window.mainloop()
