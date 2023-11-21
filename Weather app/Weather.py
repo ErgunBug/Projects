dc1a495a5b8437afe458bc33a63b68d1f0277e2e
ErgunBug <149547243+ErgunBug@users.noreply.github.com>
2023-11-21 20:45:14 +0100

    Update Weather.py
    update

--- Weather app/Weather.py
+++ Weather app/Weather.py
@@ -1,3 +1,17 @@
 import requests
-response = requests.get("https://www.weatherapi.com/my/fields.aspx")
-print(response.status_code)
+
+api_key =  '30d4741c779ba94c470ca1f63045390a'
+
+
+user_input = input("Stadt eingeben: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json() ["cod"] == "404":
+    print("Stadt nicht gefunden!")
+else:
+    weather  = weather_data.json() ["weather"] [0] ["main"]
+    temp = round(weather_data.json()["main"] ["temp"])
+    print(f"Das Wetter in {user_input} ist: {weather}")
+    print(f"Die Temperatur in {user_input} ist: {temp}")
